d587859fe1e47495c6a5c3d3511243c292a256de
Habibou Moussa <habibou997@gmail.com>
2023-12-04 08:55:30 +0100

    HERO SECTION DONE
    done with the hero section just they are some fontawesome icons that are not working and the didn't add the certificate part yet

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       colors: {
         "nav-el-color": "#2C2A80",
         "cocoa-color": "#282668",
+        "call-action": "#A6EBE3",
+        "hero-text-color": "#475569",
+        "impact-funderBTN-color": "#FF2D55",
+        "impact-deliverBTN-color": "#62C9A4",
       },
     },
   },
